112e6befeec7fa499fb58aa54a58f1a78dfaac3d
colbetyr000 <Colbetyr000@warren.k12.in.us>
2021-11-08 11:42:59 -0500

    unit project 2

--- RPGCharacterGUI/src/RPGCharacterGUI.java
+++ RPGCharacterGUI/src/RPGCharacterGUI.java
@@ -1,2 +1,15 @@
-public class RPGCharacterGUI {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+
+    JFrame  window = new JFrame( "RPG Character");;
+    JPanel panel = new JPanel();
+    JButton stat agility = new JButton("agility");
+    JButton stat strength = new JButton("Strength");
+    JButton stat defense = new JButton("defense");
+    
+    }
 }
